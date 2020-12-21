7f379513b0f26450d41ad8703684134b5fc205d2
Kamilavrhcp <kamilavrhcp@hotmail.com>
2020-12-20 23:32:58 -0300

    lesson

--- Bloco-4/4.4/palindromo.js
+++ Bloco-4/4.4/palindromo.js
@@ -8,3 +8,7 @@ let reverse = string.split('').reverse().join('');
 }
 console.log(palindromo('arara'));
 console.log(palindromo('desenvolvimento'));
+
+
+// Crie uma função que receba uma string e retorne true se for um palíndromo, ou false, se não for.//
+/* Exemplo de palídromo: arara */
