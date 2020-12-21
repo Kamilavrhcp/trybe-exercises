27ded65af820e5c4269616bb2309a57df43dae6b
Kamilavrhcp <kamilavrhcp@hotmail.com>
2020-12-20 23:31:14 -0300

    lesson

--- Bloco-4/4.4/palindromo.js
+++ Bloco-4/4.4/palindromo.js
@@ -1,3 +1,10 @@
-function palindromo() {
-
+function palindromo(string) {
+let reverse = string.split('').reverse().join('');
+  if(reverse === string) {
+    return true;
+  } else {
+  return false;
+  }
 }
+console.log(palindromo('arara'));
+console.log(palindromo('desenvolvimento'));
