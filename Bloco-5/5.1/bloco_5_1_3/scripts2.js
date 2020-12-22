a724452e09de6418523a02249a601c46bb05173a
Kamilavrhcp <kamilavrhcp@hotmail.com>
2020-12-22 16:29:19 -0300

    lesson-5.1

---
+++ Bloco-5/5.1/bloco_5_1_3/scripts2.js
@@ -0,0 +1,9 @@
+let titulo = document.querySelectorAll('#header-container')[0].style.background = 'green';
+let boardEmergency = document.getElementsByClassName('emergency-tasks')[0].style.background ='pink';
+let emergencyBlack = document.getElementsByTagName('h3')[0].style.background = 'purple';
+let emergencyBlack2 = document.getElementsByTagName('h3')[1].style.background = 'purple';
+
+let noEmergency = document.getElementsByClassName('no-emergency-tasks')[0].style.background ='yellow';
+let noEmergencyBlack = document.getElementsByTagName('h3')[2].style.background = 'black';
+let noEmergencyBlack2 = document.getElementsByTagName('h3')[3].style.background = 'black';
+
