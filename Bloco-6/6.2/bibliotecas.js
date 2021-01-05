e13d6cff97e16314f2ba9962505138a6169bfbd2
Kamilavrhcp <kamilavrhcp@hotmail.com>
2021-01-05 18:17:31 -0300

    lesson: exerciciosExtras

---
+++ Bloco-6/6.2/bibliotecas.js
@@ -0,0 +1,7 @@
+//usando a biblioteca moment:
+
+let moment = require('moment');
+let brasilTetra = moment('17071994', 'DDMMYYYY');
+let fromNow = brasilTetra.fromNow();
+
+console.log('Ouvimos o "É TETRA!!" HÁ: ' + fromNow);
