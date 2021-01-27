3fd05edc3fc8ed64f09f935a2808cc851ca18409
Kamilavrhcp <kamilavrhcp@hotmail.com>
2021-01-27 20:37:41 -0300

    Lesson: 9.1

---
+++ Bloco-9/9.1/callbacks2.js
@@ -0,0 +1,5 @@
+const assert = require('assert');
+
+const userFullName = ({ firstName, lastName }) => `Hello!My names is ${firstName} ${lastName}`;
+
+const userNationality = ({ firstName, nationality}) => `${firstName} is ${nationality}`;
